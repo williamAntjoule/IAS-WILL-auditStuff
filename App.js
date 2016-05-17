0aa3bb06f42e9482241b2c76cb94881c0d5ccfa2
williamantjoule <eeeeee@wsfdfghfgh.com>
2016-05-17 23:41:51 +0000

    Changed scope to Intativite - looking to show parent info

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     
-    
-    
     launch: function () {
         console.log("test of rallyAppBuilder");
         this._loadData();  
@@ -12,33 +10,40 @@ Ext.define('CustomApp', {
     _loadData: function(){
            var mystore = Ext.create('Rally.data.wsapi.Store', {
            model: 'PortfolioItem',
-//            fetch: true,
-            fetch: ['formattedID', 'Name', 'ScheduleState'],
+            fetch: ['formattedID', 'Name', 'DirectChildrenCount','CreationDate', 'c_Team'],
             autoLoad: true,
+            filters: [
+                {
+                    property: 'CreationDate',
+                    operator: '=',
+                    value: 'LastYear'
+                }
+            ],
+            sorters:[
+				{
+        			property: 'CreationDate',
+        			direction: 'DESC'
+				}
+			],
             listeners: {
                 load: function(myStore, myData, success){
                     console.log('get data!', myStore, myData, success);
                     this._loadGrid(myStore);
                 },
                 scope: this
-            
             }
         });
     },
     
     _loadGrid: function(myStoryStore){
         var myGrid = Ext.create('Rally.ui.grid.Grid',{
-                        store: myStoryStore,
-                        columnCfgs:[
-                            "FormattedID", "Name"
-                        ]
-                    }); 
-                    
-                    this.add(myGrid);
-                    
-                    console.log('what is this?',this);
+            store: myStoryStore,
+            columnCfgs:[
+                "FormattedID", "Name", "DirectChildrenCount", "CreationDate", "c_Team"
+            ]
+        }); 
+        this.add(myGrid);
+        console.log('what is this?',this);
     }
-    
-
 });
  
